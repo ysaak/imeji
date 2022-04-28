ed8ee71b2c24fc2c96ca4a98be02dbefa307b928
Damien ROTH <damien.roth@gmail.com>
2022-04-28 08:06:54 +0200

    fix resource locations paths

--- src/main/java/ysaak/imeji/config/WebMvcConfig.java
+++ src/main/java/ysaak/imeji/config/WebMvcConfig.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 @Configuration
 @EnableWebMvc
@@ -20,8 +21,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/wallpapers/full/**").addResourceLocations("file:" + applicationConfig.getWallpaperStoragePath()).setCachePeriod(0);
-        registry.addResourceHandler("/wallpapers/thumbnail/**").addResourceLocations("file:" + applicationConfig.getThumbnailStoragePath()).setCachePeriod(0);
+        registry.addResourceHandler("/wallpapers/full/**")
+                .addResourceLocations("file:" + applicationConfig.getWallpaperStoragePath().toAbsolutePath() + File.separator)
+                .setCachePeriod(0);
+
+        registry.addResourceHandler("/wallpapers/thumbnail/**")
+                .addResourceLocations("file:" + applicationConfig.getThumbnailStoragePath().toAbsolutePath() + File.separator)
+                .setCachePeriod(0);
+
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/").setCachePeriod(0);
     }
 }
